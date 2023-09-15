1d47ece48d839d4de88cbadd17f7f661ff697f4a
ShepLT1 <luke.t.shepherd@gmail.com>
2023-09-14 22:37:41 -0700

    Create NonLand class with CardType enum

---
+++ src/main/java/com/example/mtg/NonLand.java
@@ -0,0 +1,56 @@
+package com.example.mtg;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.*;
+
+@Entity
+public class NonLand extends Card {
+
+    public enum CardType {
+        INSTANT, SORCERY, ARTIFACT, ENCHANTMENT, CREATURE
+    }
+
+    private @Id @GeneratedValue Long id;
+    private List<Color> manaCost;
+    private CardType type;
+
+    public NonLand(String name, List<Color> manaCost, CardType type) {
+        this(name, new ArrayList<>(), manaCost, type);
+    }
+
+    public NonLand(String name, List<String> abilities, List<Color> manaCost, CardType type) {
+        super(name, abilities);
+        Collections.sort(manaCost);
+        this.manaCost = manaCost;
+        this.type = type;
+    }
+
+    public int getManaValue() {
+        return this.manaCost.size();
+    }
+
+    public String getManaCost() {
+
+        StringBuilder output = new StringBuilder();
+        for (Color color : new HashSet<>(this.manaCost)) {
+            if (!output.toString().equals("")) {
+                output.append(", ");
+            }
+            output.append(Collections.frequency(this.manaCost, color)).append(" ").append(color.toString());
+        }
+        return output.toString();
+    }
+
+    @Override
+    public List<Color> getColors() {
+        return new ArrayList<>(new LinkedHashSet<>(this.manaCost));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("Card type = %s%nMana Cost = %s%n", this.type, getManaCost());
+    }
+}
