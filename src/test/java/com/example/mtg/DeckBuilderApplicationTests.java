cbad47df5f1f1ca68981588797870f2cddfc7b09
ShepLT1 <luke.t.shepherd@gmail.com>
2023-09-14 17:05:05 -0700

    initial spring app

---
+++ src/test/java/com/example/mtg/DeckBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
