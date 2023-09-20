644910bcb0e47a69eef53eb011f75d61e1513215
ShepLT1 <luke.t.shepherd@gmail.com>
2023-09-19 21:47:09 -0700

    Add Color enum JSON converters

--- src/main/java/com/mtg/Color.java
+++ src/main/java/com/mtg/Color.java
@@ -1,5 +1,23 @@
 package com.mtg;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Color {
-    ANY_COLOR, COLORLESS, WHITE, BLUE, BLACK, RED, GREEN
+    ANY, COLORLESS, WHITE, BLUE, BLACK, RED, GREEN;
+
+    @JsonCreator
+    public static Color fromText(String text) {
+        for (Color c : Color.values()) {
+            if (c.name().equals(text.toUpperCase())) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
+
 }
