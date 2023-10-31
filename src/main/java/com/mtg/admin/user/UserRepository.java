02f22070da24cf5c7f3bc68787387410f36bbe59
ShepLT1 <luke.t.shepherd@gmail.com>
2023-10-31 11:03:19 -0700

    Add user repository

---
+++ src/main/java/com/mtg/admin/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mtg.admin.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+}
